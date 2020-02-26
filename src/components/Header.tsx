28274b50ea7208beff122159758ff82531595dfc
MohnishKalia <mohnish.kalia@gmail.com>
2020-02-26 13:33:38 -0600

    State and Presets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Header = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <span className="navbar-brand mb-0 h1">Navbar</span>
+            <span className="navbar-brand mb-0 h1">Foil Sim</span>
         </nav>
     )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
-import { GlobalContext, State } from '../context/GlobalContext'
+import { GlobalContext, State, Action } from '../context/GlobalContext'
 
 const Main = () => {
     return (
         <main className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <Preview />
+                    {/* <Preview /> */}
                     <MainForm />
+                    <Presets />
                 </div>
                 <div className="col-md-6">
                     <Settings />
@@ -20,44 +21,98 @@ const Main = () => {
 
 export const MainForm = () => {
     const [state, dispatch] = useContext(GlobalContext);
-    const InputChanger: React.FC<{ changing: keyof State }> = ({ changing }) =>
-        <>
-            <label htmlFor={changing} className="text-capitalize">{changing}</label>
-            <input className="form-control" type="range" name={changing} min={0.1} max={19.9} defaultValue={state[changing]} onChange={e => dispatch({ type: changing, value: e.target.valueAsNumber })} step={0.1} />
-        </>
+
     return (
-        <div>
-            {(Object.keys(state) as (keyof State)[]).map(
-                (k) => <InputChanger key={k} changing={k} />)
-            }
-        </div>
+        <form>
+            <InputChanger changing={'angle'} min={-20} max={20} state={state} dispatch={dispatch} />
+            <InputChanger changing={'camber'} min={-20} max={20} state={state} dispatch={dispatch} />
+            <InputChanger changing={'chord'} min={-20} max={20} state={state} dispatch={dispatch} />
+            <InputChanger changing={'span'} min={5} max={123.8} state={state} dispatch={dispatch} />
+            <InputChanger changing={'speed'} min={0} max={250} state={state} dispatch={dispatch} />
+            <InputChanger changing={'thickness'} min={1} max={20} state={state} dispatch={dispatch} />
+        </form>
     );
 }
 
+interface Input { changing: keyof State, min: number, max: number, state: State, dispatch: React.Dispatch<Action> };
 
+const InputChanger: React.FC<Input> = ({ changing, min, max, state, dispatch }) =>
+    <div className="form-group">
+        <label htmlFor={changing} className="text-capitalize">{changing}</label>
+        <input className="form-control" type="range" name={changing} min={min} max={max} value={state[changing]}
+            onChange={e => dispatch({ type: changing, value: e.target.valueAsNumber })} step={0.1} />
+    </div>
 
 export const DataDisplay = () => {
     const [state] = useContext(GlobalContext);
     return (
         <div>
-            <h1>hello from display</h1>
+            <h1>Display</h1>
             {Object.entries(state).map(([k, v]) =>
-                <div>
-                    <p>key : {k}</p>
-                    <p>value: {v}</p>
+                <div key={k}>
+                    <p>{k} : {v}</p>
                 </div>
             )}
         </div>
     )
 }
 
+interface Datum { text: string, angle: number, camber: number, thickness: number };
 
-export const Preview = () => {
+export const Presets = () => {
+    const [, dispatch] = useContext(GlobalContext);
+    const data: Datum[] = [
+        {
+            text: 'Symmetric',
+            angle: 0,
+            camber: 0,
+            thickness: 12.5
+        },
+        {
+            text: 'High Camber',
+            angle: 9,
+            camber: 15,
+            thickness: 12.5
+        },
+        {
+            text: 'Flat Plate',
+            angle: 9,
+            camber: 15,
+            thickness: 1
+        },
+        {
+            text: 'Flat Bottom',
+            angle: 7,
+            camber: 5,
+            thickness: 12.5
+        },
+        {
+            text: 'Negative Camber',
+            angle: -7,
+            camber: -5,
+            thickness: 12.5
+        },
+    ];
     return (
-        <img src="https://via.placeholder.com/360" alt="C/O https://placeholder.com/#How_To_Use_Our_Placeholders" />
+        <div className="btn-group" role="group">
+            {data.map(val => <Btn data={val} dispatch={dispatch} />)}
+        </div>
     )
 }
 
+const Btn: React.FC<{ data: Datum, dispatch: React.Dispatch<Action> }> = ({ data: { angle, camber, thickness, text }, dispatch }) =>
+    <button type="button" className="btn btn-secondary" onClick={() => {
+        dispatch({ type: 'angle', value: angle });
+        dispatch({ type: 'camber', value: camber });
+        dispatch({ type: 'thickness', value: thickness });
+    }}>{text}</button>
+
+export const Preview = () => {
+    return (
+        <img className="img-fluid text-center" src="https://via.placeholder.com/320"
+            alt="C/O https://placeholder.com/#How_To_Use_Our_Placeholders" />
+    )
+}
 
 export const Settings = () => {
     return (
@@ -66,8 +121,5 @@ export const Settings = () => {
         </div>
     )
 }
-
-
-
 
 export default Main
